02bec94492819e54ceb574fade779f0df9b9bc8d
MagiscCow <ddyshlovskyi@gmail.com>
2024-09-24 12:48:11 +0200

    sigma

--- back_end.py
+++ back_end.py
@@ -2,17 +2,27 @@ def leggSammen():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
     sum = int(tall1) + int(tall2)
-    print(f'{tall2} + {tall2} = {sum}')
+    print(f'{tall1} + {tall2} = {sum}')
 
 
 def trekkFra():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
     diff = int(tall1) - int(tall2)
-    print(f'{tall2} - {tall2} = {diff}')
+    print(f'{tall1} - {tall2} = {diff}')
 
-
-''' Her er noen påbegynte tanker til utvidelse
 def gange():
-    produkt = 3*3
-    TODO: Gjøre ferdig '''
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    produkt = int(tall1) * int(tall2)
+    print(f'{tall1} * {tall2} = {produkt}')
+    
+def dele():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
+    while tall2 != "0":
+        result = int(tall1) / int(tall2)
+        print(f'{tall1} / {tall2} = {result}')
+    if tall2 == "0":
+        print("[ERROR] - Kan ikke dele på 0!")
+        return
