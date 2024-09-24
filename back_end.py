cb2ddbbaaccfa26ce15ad75bb05a610501b030f5
MagiscCow <ddyshlovskyi@gmail.com>
2024-09-24 13:09:08 +0200

    Update#3 backend

--- back_end.py
+++ back_end.py
@@ -18,10 +18,9 @@ def gange():
     print(f'{tall1} * {tall2} = {produkt}')
     
 def dele():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
+    tall1 = int(input("Skriv inn det første tallet: "))
+    tall2 = int(input("Skriv inn det andre tallet: "))
     result = int(tall1) / int(tall2)
     print(f'{tall1} / {tall2} = {result}')
-    if int(tall2) <= "0":
+    if int(tall2) <= 0:
         print("[ERROR] - Kan ikke dele på 0!")
-        return
