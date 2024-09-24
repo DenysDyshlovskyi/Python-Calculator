1a2e26f33a64074c1cd57951bbfefc62f4dc2447
MagiscCow <ddyshlovskyi@gmail.com>
2024-09-24 13:02:48 +0200

    Fixed Dele endless loop

--- back_end.py
+++ back_end.py
@@ -20,9 +20,8 @@ def gange():
 def dele():
     tall1 = input("Skriv inn det første tallet: ")
     tall2 = input("Skriv inn det andre tallet: ")
-    while tall2 != "0":
-        result = int(tall1) / int(tall2)
-        print(f'{tall1} / {tall2} = {result}')
-    if tall2 == "0":
+    result = int(tall1) / int(tall2)
+    print(f'{tall1} / {tall2} = {result}')
+    if int(tall2) <= 0:
         print("[ERROR] - Kan ikke dele på 0!")
         return
